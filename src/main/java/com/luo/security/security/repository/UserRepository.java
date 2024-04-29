e359ae063c974267ac03a6d4724f76b213e6f1b2
lingluo1hao <37951931+lingluo1hao@users.noreply.github.com>
2024-04-29 09:35:14 +0800

    Update UserRepository.java

--- src/main/java/com/luo/security/security/repository/UserRepository.java
+++ src/main/java/com/luo/security/security/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * User Repository 接口.
  * 
  * @since 1.0.0 2017年7月16日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 public interface UserRepository extends CrudRepository<User, Long> {
 
